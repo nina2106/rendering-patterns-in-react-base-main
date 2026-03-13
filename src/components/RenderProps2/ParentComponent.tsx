9ede3f811415c758bddbba7e7f795b71fcce61a3
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-12 20:38:28 -0500

    carolina

--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,17 @@
-const ParentComponent = () => {
+import React, { useState } from "react";
+
+interface ParentComponentProps {
+    render: (data: string[]) => React.ReactNode;
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+    const [data] = useState<string[]>(["Apple 🍎", "Banana 🍌", "Orange 🍊"]);
+
+    return (
+        <div>
+            {render(data)}
+        </div>
+    );
 };
 
 export default ParentComponent;
