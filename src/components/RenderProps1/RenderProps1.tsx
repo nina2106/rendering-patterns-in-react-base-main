54a978ae703c18e4086cbcaf06bd0d1021566678
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-12 20:02:14 -0500

    primer commit

---
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+};
+
+export const ChildComponent: React.FC<ChildComponentProps> = ( { render }) => {
+  const name = "Teffcode";
+  return (
+    <div>
+    {render(name)}
+    </div>
+  );
+};
+
+export const ParentComponent = () => {
+  return (
+    <ChildComponent render={(name) => <p>Hello, {name}  !</p>} />
+  );
+};
