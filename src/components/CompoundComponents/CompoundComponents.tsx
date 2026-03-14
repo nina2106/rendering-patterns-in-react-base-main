83994814713614849175174182200a408b41541f
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-13 23:26:40 -0500

    carolina

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,17 @@
+import Tab from "./Tab"
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <div>
+      <Tab label="Girafa">
+        🦒
+      </Tab>
+      <Tab label="Delfin">
+        🐬
+      </Tab>
+      <Tab label="Lagarto">
+        🦎
+      </Tab>
+    </div>
   );
 };
