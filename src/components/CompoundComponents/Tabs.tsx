ce81cdc9d8e77c0f32fb8c51aee39715ca4676ae
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-13 23:36:43 -0500

    carolina

--- src/components/CompoundComponents/Tabs.tsx
+++ src/components/CompoundComponents/Tabs.tsx
@@ -1,8 +1,18 @@
+import React, { Children, useState } from "react";
 import classes from "./CompoundComponents.module.css";
 
 const Tabs = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const tabElements = React.Children.toArray(Children)
+  
   return (
     <div className={classes.Tabs}>
+
     </div>
   );
 };
